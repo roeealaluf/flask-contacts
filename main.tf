0fb0f747937f0f45538f7800be3d5e700cf8e171
roeealaluf <162418635+roeealaluf@users.noreply.github.com>
2024-05-28 22:59:50 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -31,3 +31,69 @@ resource "vultr_instance" "jenkins" {
   tags = ["ubuntu","jenkins"] 
   backups = "disabled"
 }
+
+
+provider "docker" {
+  host = "unix:///var/run/docker.sock"
+}
+
+resource "docker_image" "contacsweb_image" {
+  name         = "your-web-image:latest"
+  keep_locally = false
+}
+
+resource "docker_container" "contacsweb_container" {
+  name  = "contacsweb"
+  image = docker_image.contacsweb_image.latest
+
+  ports {
+    internal = 80
+    external = 8080
+  }
+
+  env = [
+    "DB_HOST=contacsdb",
+    "DB_PORT=5432",
+    "DB_USER=youruser",
+    "DB_PASSWORD=yourpassword"
+  ]
+
+  depends_on = [docker_container.contacsdb_container]
+}
+
+resource "docker_image" "contacsdb_image" {
+  name         = "contactsweb:latest"
+  keep_locally = false
+}
+
+resource "docker_container" "contacsdb_container" {
+  name  = "contacsdb"
+  image = docker_image.contacsdb_image.latest
+
+  ports {
+    internal = 5432
+    external = 5432
+  }
+
+  env = [
+    "POSTGRES_USER=youruser",
+    "POSTGRES_PASSWORD=yourpassword",
+    "POSTGRES_DB=yourdatabase"
+  ]
+}
+
+output "contacsweb_container_id" {
+  value = docker_container.contacsweb_container.id
+}
+
+output "contacsdb_container_id" {
+  value = docker_container.contacsdb_container.id
+}
+
+output "contacsweb_container_ip" {
+  value = docker_container.contacsweb_container.ip_address
+}
+
+output "contacsdb_container_ip" {
+  value = docker_container.contacsdb_container.ip_address
+}
